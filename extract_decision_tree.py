e1ba72b020afa6c1044356b19c1dc3744b78a389
Victor Barua <victor.barua@gmail.com>
2020-11-11 11:28:03 -0800

    Building decision tree data structure

---
+++ extract_decision_tree.py
@@ -0,0 +1,48 @@
+import csv
+from dataclasses import dataclass
+from typing import Optional
+
+from bs4 import BeautifulSoup
+
+data_path = "tenants-talk/{}.html"
+
+@dataclass
+class ChoiceEdge:
+    decision: str
+    link: str
+    assist: Optional[str]
+
+@dataclass
+class DecisionNode:
+    uri: str
+    prompt: str
+    choices: list[ChoiceEdge]
+
+def process(uri: str, html_doc: str) -> DecisionNode:
+    soup = BeautifulSoup(html_doc, "html.parser")
+    jumbotron = soup.find("div", {"class": "jumbotron"})
+    prompt = jumbotron.h2.text
+    raw_choices = jumbotron.div.ul.find_all("li")
+    choices = [process_choice (rc) for rc in raw_choices]
+    return DecisionNode(uri, prompt, choices)
+
+def process_choice(raw_choice) -> ChoiceEdge:
+    as_ = raw_choice.find_all("a")
+    decision = as_[0].text.strip()
+    link = as_[0].attrs["href"]
+    assist = None
+    if len(as_) == 2:
+        assist = as_[1].attrs["data-content"].strip()
+    return ChoiceEdge(decision, link, assist)
+
+if __name__ == "__main__":
+    for i in range (1, 45):
+        with open(data_path.format(i), 'r') as f:
+            html_doc = f.read()
+
+        uri = "/contest/{}".format(i)
+
+        print("\nProcessing: {}".format(uri))
+        decision_node = process(uri, html_doc)
+        print(decision_node)
+
