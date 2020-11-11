c38bdb839dd1c88a79fa0355c57f456e3d715d6c
Victor Barua <victor.barua@gmail.com>
2020-11-11 14:58:14 -0800

    Visualizing the tenants-talk decision tree

---
+++ graphs/render.sh
@@ -0,0 +1,4 @@
+dot -Tsvg advice_evictions.dot -o advice_evictions.svg
+dot -Tsvg advice_full.dot -o advice_full.svg
+dot -Tsvg advice_repairs.dot -o advice_repairs.svg
+dot -Tsvg advice_sec_dep.dot -o advice_sec_dep.svg
