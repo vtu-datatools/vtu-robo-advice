00367f5c1266e33cc560d458c557e5e173b558a0
Victor Barua <victor.barua@gmail.com>
2021-01-02 20:48:07 -0800

    Make app take up the entire screen

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -15,7 +15,7 @@ const App: FunctionalComponent = () => {
           text-3xl text-gray-100">
             VTU - Tenant Advice
       </header>
-      <div className="px-8 py-2 bg-blue-400">
+      <div className="h-screen px-8 py-2 bg-blue-400">
         <div className="text-2xl text-center pb-2">Navigate Your Eviction</div>
         <Router>
           <Route<PromptProps> path="/" component={Prompt} prompts={PROMPTS} promptId="46ed67"  />
