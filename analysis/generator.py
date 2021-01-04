172da95a5f42211c6d74893b44db9f097e8ded3f
Victor Barua <victor.barua@gmail.com>
2021-01-03 18:45:08 -0800

    Render .dot files from our decision trees

--- analysis/generator.py
+++ analysis/generator.py
@@ -1,8 +1,7 @@
 import csv
 from dataclasses import dataclass
 import json
-import hashlib
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional
 
 URI = str
 NodeId = str
@@ -39,16 +38,54 @@ def parse_rows() -> List[Row]:
     return rows
 
 
-def main() -> None:
-    rows: List[Row] = parse_rows()
-    groupedRows: Dict[NodeId, List[Row]] = {}
+def group_rows(rows: List[Row]) -> Dict[NodeId, List[Row]]:
+    grouped_rows: Dict[NodeId, List[Row]] = {}
     for row in rows:
-        r = groupedRows.get(row.node_id, [])
+        r = grouped_rows.get(row.node_id, [])
         r.append(row)
-        groupedRows[row.node_id] = r
+        grouped_rows[row.node_id] = r
+    return grouped_rows
 
+
+def gen_graph(rows: List[Row]) -> str:
+    grouped_rows: Dict[NodeId, List[Row]] = group_rows(rows)
+
+    buffer = []
+    buffer.append("digraph decision_tree {")
+
+    # Render Internal Nodes
+    for gr in grouped_rows.values():
+        row: Row = gr[0]
+        buffer.append(
+            '  "{}" [label="{}\\n{}"]'.format(row.node_id, row.node_id, row.prompt)
+        )
+
+    # Render External Nodes
+    external_links = set([row.external_link for row in rows if row.external_link])
+    for el in external_links:
+        buffer.append('  "{}" [label="{}" shape="diamond"]'.format(el, el))
+
+    # Render Edges
+    for (node_id, grs) in grouped_rows.items():
+        for row in grs:
+            link: str
+            if row.node_link:
+                link = row.node_link
+            elif row.external_link:
+                link = row.external_link
+            else:
+                link = ""
+            buffer.append(
+                '  "{}" -> "{}" [label="{}"]'.format(node_id, link, row.choice)
+            )
+
+    buffer.append("}")
+    return "\n".join(buffer)
+
+
+def gen_data(grouped_rows: Dict[NodeId, List[Row]]) -> str:
     output = dict()
-    for (key, rows) in groupedRows.items():
+    for (key, rows) in grouped_rows.items():
         cs = []
         for row in rows:
             temp = {"choiceText": row.choice}
@@ -58,10 +95,22 @@ def main() -> None:
                 temp["linkOut"] = row.external_link
             cs.append(temp)
 
-        d = {"id": rows[0].node_id, "promptText": rows[0].prompt, "choices": cs}
-        output[rows[0].node_id] = d
+        d = {"id": key, "promptText": rows[0].prompt, "choices": cs}
+        output[key] = d
+    return json.dumps(output, indent=2)
 
-    print(json.dumps(output, indent=2))
+
+def main() -> None:
+    rows: List[Row] = parse_rows()
+    grouped_rows: Dict[NodeId, List[Row]] = group_rows(rows)
+
+    graph = gen_graph(rows)
+    with open("trees/evictions_tree.dot", "w") as f:
+        f.write(graph)
+
+    data = gen_data(grouped_rows)
+    with open("trees/evictions_tree.json", "w") as f:
+        f.write(data)
 
 
 if __name__ == "__main__":
